7b0c5f050dce65aaacf795b3198ae37854ef9501
TranHungKt <tranhung26122612@gmail.com>
2020-08-05 10:34:15 +0700

    fix userSchema

--- Backend/modal/userSchema.js
+++ Backend/modal/userSchema.js
@@ -31,6 +31,24 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
