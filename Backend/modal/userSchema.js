f309a9bc5b7630b1cb594c96bca7750185452638
TranHungKt <tranhung26122612@gmail.com>
2020-08-12 14:26:22 +0700

    Login

--- Backend/modal/userSchema.js
+++ Backend/modal/userSchema.js
@@ -57,7 +57,7 @@ userSchema.pre("save", function (next) {
 });
 
 userSchema.methods.comparePassword = function (plainPassword, cb) {
-  bcrypt.comparePassword(plainPassword, this.password, function (err, isMatch) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
     if (err) return err;
     cb(null, isMatch);
   });
--- Backend/routes/user/authAction/login.js
+++ Backend/routes/user/authAction/login.js
@@ -4,5 +4,37 @@ const jwt = require("jsonwebtoken");
 const { User } = require("../../../modal/userSchema");
 const accessTokenSecret = process.env.accessTokenSecret;
 const bcrypt = require("bcryptjs");
+const { validateLogin } = require("../../../utils/validator");
+router.post("/", (req, res) => {
+  const { email, password } = req.body;
+  const tempUser = {
+    email,
+    password,
+  };
+
+  const { errors, valid } = validateLogin(tempUser);
+  if (!valid) return res.status(400).json(errors);
+
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(400)
+          .json({ message: "Your email is not registered" });
+      }
+      user.comparePassword(password, (err, isMatch) => {
+        if (err) return res.status(400).json({ err });
+        if (!isMatch) {
+          return res.status(401).json({ message: "Password is not match" });
+        }
+        let token = jwt.sign(
+          { email: email, password: password },
+          accessTokenSecret
+        );
+        return res.status(200).json({ token });
+      });
+    })
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
